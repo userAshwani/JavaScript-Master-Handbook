152e6298e75ef81b375cee91d540813dc4cf67fa
Ashwani Tiwari <codeashwani@gmail.com>
2026-08-18 23:46:44 +0530

    update-readme.js

---
+++ update-readme.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const questionsDir = path.join(__dirname, 'question-practice');
+const projectsDir = path.join(__dirname, 'projects');
+
+// Updated to count all files (ignoring hidden files)
+const getCount = (dirPath) => {
+  if (!fs.existsSync(dirPath)) return 0;
+  return fs.readdirSync(dirPath).filter(item => {
+    return !item.startsWith('.'); 
+  }).length;
+};
+
+const questionsCount = getCount(questionsDir);
+const projectsCount = getCount(projectsDir);
+
+const readmePath = path.join(__dirname, 'README.md');
+let readmeContent = fs.readFileSync(readmePath, 'utf8');
+
+const questionsRegex = /[\s\S]*?/;
+const questionsReplacement = `\n\n`;
+
+const projectsRegex = /[\s\S]*?/;
+const projectsReplacement = `\n\n`;
+
+readmeContent = readmeContent.replace(questionsRegex, questionsReplacement);
+readmeContent = readmeContent.replace(projectsRegex, projectsReplacement);
+
+fs.writeFileSync(readmePath, readmeContent);
+console.log(`Updated README: ${questionsCount} questions, ${projectsCount} projects.`);
